f06c3bdf272c5ccb166e60d6f9ecc73f229af8c8
Henrique Tavares <ihenrits@gmail.com>
2021-08-09 22:47:31 -0300

    utilizando dynamix import para scripts que so funcionam no browser

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,11 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function Map() {
   return (
-    <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
