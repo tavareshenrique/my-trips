642954a4439252ccbde109e681ba1263ca4f5ddb
Henrique Tavares <ihenrits@gmail.com>
2021-09-22 19:59:03 -0300

    configurando o visual com mapbox

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -5,6 +5,24 @@ import { IMapProps } from './@interfaces'
 
 export type MapProps = IMapProps
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
+
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
+
 export default function Map({ places }: IMapProps) {
   const router = useRouter()
 
@@ -14,10 +32,7 @@ export default function Map({ places }: IMapProps) {
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
 
       {places?.map(({ id, location, name, slug }) => {
         const { latitude, longitude } = location
